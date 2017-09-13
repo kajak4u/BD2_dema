356e705048c037f3e44c84d55c0d67a6ebbddf5a
Łukasz <ukasz@N50>
2017-09-13 13:49:37 +0200

    ExaminationsDictionariesDetails - zapisywanie zmian do bazy+podpowiadanie.

--- BD2_demaOkien.BizzLayer/BizLayer.cs
+++ BD2_demaOkien.BizzLayer/BizLayer.cs
@@ -7,216 +7,221 @@ using BD2_demaOkien.Data;
 
 namespace BD2_demaOkien
 {
-    class BizLayer
-    {
-        public static void Main() { }
-    }
-
-    public class AllPatientsData
-    {
-        public int Patient_id;
-        public string First_name;
-        public string Last_name;
-        public string PESEL;
-        public string Phone_number;
-        public string Address;
-    }
-
-    public class PatientData
-    {
-        public int Patient_id;
-        public string First_name;
-        public string Last_name;
-        public string PESEL;
-        public string Phone_number;
-        public string Street;
-        public string City;
-        public int HouseNo;
-        public int? FlatNo;
-    }
-
-    static public class Worker
-    {
-        public static Data.Worker getWorker(String userLogin, String userPassword)
-        {
-            using (var Db = new BD2_2Db())
-            {
-                var worker = Db.Worker.ToList()
-                    .Where(w => w.Login == userLogin && w.Password == userPassword)
-                    .FirstOrDefault();
-                return worker;
-            }
-        }
-    }
-
-    static public class Visit
-    {
-        //takes all patients from db
-        public static List<AllPatientsData> getAllPatients()
-        {
-            using (var Db = new BD2_2Db())
-            {
-                var patients = from patient in Db.Patient
-                               join address in Db.Address
-                               on patient.address_id equals address.Address_id
-                               select new AllPatientsData
-                               {
-                                   Patient_id = patient.Patient_id,
-                                   First_name = patient.First_name,
-                                   Last_name = patient.Last_name,
-                                   PESEL = patient.PESEL,
-                                   Phone_number = patient.Phone_number,
-                                   Address = address.Flat_number != null ? address.City + " " + address.Street + " " + address.House_number + " " + address.Flat_number : address.City + " " + address.Street + " " + address.House_number
-                               };
-                //var patients = Db.Patient.ToList();
-                return patients.ToList();
-            }
-        }
-
-        //gets one patient from database by all data 
-        public static List<AllPatientsData> getPatientByAllData(String FirstName, String LastName, String Pesel)
-        {
-            using (var Db = new BD2_2Db())
-            {
-                var patient = from patients in Db.Patient
-                              join address in Db.Address
-                              on patients.address_id equals address.Address_id
-                              where patients.First_name.Contains(FirstName) && patients.Last_name.Contains(LastName) && patients.PESEL.Contains(Pesel)//FirstName == patients.First_name && LastName == patients.Last_name //&& Pesel == patients.PESEL
-                              select new AllPatientsData
-                              {
-                                  Patient_id = patients.Patient_id,
-                                  First_name = patients.First_name,
-                                  Last_name = patients.Last_name,
-                                  PESEL = patients.PESEL,
-                                  Address = address.Flat_number != null ? address.City + " " + address.Street + " " + address.House_number + " " + address.Flat_number : address.City + " " + address.Street + " " + address.House_number
-                              };
-                //var patients = Db.Patient.ToList();
-                return patient.ToList();
-            }
-        }
-
-        public static PatientData getPatientById(int id)
-        {
-            using (var Db = new BD2_2Db())
-            {
-                var patient = from patients in Db.Patient
-                              join address in Db.Address
-                              on patients.address_id equals address.Address_id
-                              where patients.Patient_id == id
-                              select new PatientData
-                              {
-                                  Patient_id = patients.Patient_id,
-                                  First_name = patients.First_name,
-                                  Last_name = patients.Last_name,
-                                  PESEL = patients.PESEL,
-                                  Phone_number = patients.Phone_number,
-                                  City = address.City,
-                                  Street = address.Street,
-                                  HouseNo = address.House_number,
-                                  FlatNo = address.Flat_number
-                                  //address = address.Flat_number != null ? address.City + " " + address.Street + " " + address.House_number + " " + address.Flat_number : address.City + " " + address.Street + " " + address.House_number
-                              };
-                //var patients = Db.Patient.ToList();
-                return patient.FirstOrDefault();
-            }
-        }
-
-        public static void deleteUser(int id) {
-            using (var Db = new BD2_2Db())
-            {
-                var patient = Db.Patient.Where(p => p.Patient_id == id).FirstOrDefault();
-                if (patient == null)
-                    throw new Exception("No such patient!");
-
-                //Db.Address.Remove(add.First()); - adres zostaje w bazie! - co jeśli ktoś inny też go ma?
-                Db.Patient.Remove(patient);
-                Db.SaveChanges();
-            }
-
-        }
-
-
-        public static void setPatientData(String name, String surname, String pesel, String phone, String city, String street, int houseNo, int? flatNo, int? id)
-        {
-            Address address = new Address { City=city, Street=street, House_number=houseNo, Flat_number=flatNo};
-
-            Patient patient = new Patient { First_name=name, Last_name=surname, PESEL=pesel, Phone_number=phone};
-
-            using (var Db = new BD2_2Db())
-            {
-                var matchAddresses = Db.Address
-                    .Where(add =>
-                         (add.City == null ? address.City == null : add.City.Equals(address.City))
-                            && (add.Street == null ? address.Street == null : add.Street.Equals(address.Street))
-                            && add.House_number == address.House_number
-                            && (!add.Flat_number.HasValue ? !address.Flat_number.HasValue : add.Flat_number.Value == address.Flat_number.Value));
-                if (matchAddresses.Count() == 0)
-                {
-                    Db.Address.Add(address);
-                    //Db.SaveChanges(); // ważne!
-                }
-                else
-                    address = matchAddresses.First();
-
-                patient.Address = address;
-                Db.Patient.Add(patient);
-                Db.SaveChanges();
-            }
-        }
-
-        public static void editPatientData(String name, String surname, String pesel, String phone, String city, String street, int houseNo, int flatNo, int id)
-        {
-            using (var Db = new BD2_2Db())
-            {
-                var patientData = Db.Patient.Where(p => p.Patient_id == id).FirstOrDefault();
-                patientData.First_name = name;
-                patientData.Last_name = surname;
-                patientData.PESEL = pesel;
-                patientData.Phone_number = phone;
-
-                var patientAddress = Db.Address.Where(p => p.Address_id == patientData.address_id).FirstOrDefault();
-                patientAddress.City = city;
-                patientAddress.Street = street;
-                patientAddress.House_number = houseNo;
-                patientAddress.Flat_number = flatNo;
-                Db.SaveChanges();
-            }
-        }
-    }
-
-    static public class Examinations {
-
-		//public static void insertExamination(String name, String code, String result, String type)
-		//{
-		//	//Examination_dictionary exam = new Examination_dictionary { Examiantion_type = type, Examination_code = code, Examination_name = name };
-			
-		//	using (var Db = new BD2_2Db())
-		//	{
-		//		var matchAddresses = Db.Address
-		//			.Where(add =>
-		//				 (add.City == null ? address.City == null : add.City.Equals(address.City))
-		//					&& (add.Street == null ? address.Street == null : add.Street.Equals(address.Street))
-		//					&& add.House_number == address.House_number
-		//					&& (!add.Flat_number.HasValue ? !address.Flat_number.HasValue : add.Flat_number.Value == address.Flat_number.Value));
-		//		if (matchAddresses.Count() == 0)
-		//		{
-		//			Db.Address.Add(address);
-		//			//Db.SaveChanges(); // ważne!
-		//		}
-		//		else
-		//			address = matchAddresses.First();
-
-		//		patient.Address = address;
-		//		Db.Patient.Add(patient);
-		//		Db.SaveChanges();
-		//	}
-		//}
+	class BizLayer
+	{
+		public static void Main() { }
 	}
 
-    static public class Patients
-    {
+	public class AllPatientsData
+	{
+		public int Patient_id;
+		public string First_name;
+		public string Last_name;
+		public string PESEL;
+		public string Phone_number;
+		public string Address;
+	}
 
-    }
+	public class PatientData
+	{
+		public int Patient_id;
+		public string First_name;
+		public string Last_name;
+		public string PESEL;
+		public string Phone_number;
+		public string Street;
+		public string City;
+		public int HouseNo;
+		public int? FlatNo;
+	}
+
+	static public class Worker
+	{
+		public static Data.Worker getWorker(String userLogin, String userPassword)
+		{
+			using (var Db = new BD2_2Db())
+			{
+				var worker = Db.Worker.ToList()
+					.Where(w => w.Login == userLogin && w.Password == userPassword)
+					.FirstOrDefault();
+				return worker;
+			}
+		}
+	}
+
+	static public class Visit
+	{
+		//takes all patients from db
+		public static List<AllPatientsData> getAllPatients()
+		{
+			using (var Db = new BD2_2Db())
+			{
+				var patients = from patient in Db.Patient
+							   join address in Db.Address
+							   on patient.address_id equals address.Address_id
+							   select new AllPatientsData
+							   {
+								   Patient_id = patient.Patient_id,
+								   First_name = patient.First_name,
+								   Last_name = patient.Last_name,
+								   PESEL = patient.PESEL,
+								   Phone_number = patient.Phone_number,
+								   Address = address.Flat_number != null ? address.City + " " + address.Street + " " + address.House_number + " " + address.Flat_number : address.City + " " + address.Street + " " + address.House_number
+							   };
+				//var patients = Db.Patient.ToList();
+				return patients.ToList();
+			}
+		}
+
+		//gets one patient from database by all data 
+		public static List<AllPatientsData> getPatientByAllData(String FirstName, String LastName, String Pesel)
+		{
+			using (var Db = new BD2_2Db())
+			{
+				var patient = from patients in Db.Patient
+							  join address in Db.Address
+							  on patients.address_id equals address.Address_id
+							  where patients.First_name.Contains(FirstName) && patients.Last_name.Contains(LastName) && patients.PESEL.Contains(Pesel)//FirstName == patients.First_name && LastName == patients.Last_name //&& Pesel == patients.PESEL
+							  select new AllPatientsData
+							  {
+								  Patient_id = patients.Patient_id,
+								  First_name = patients.First_name,
+								  Last_name = patients.Last_name,
+								  PESEL = patients.PESEL,
+								  Address = address.Flat_number != null ? address.City + " " + address.Street + " " + address.House_number + " " + address.Flat_number : address.City + " " + address.Street + " " + address.House_number
+							  };
+				//var patients = Db.Patient.ToList();
+				return patient.ToList();
+			}
+		}
+
+		public static PatientData getPatientById(int id)
+		{
+			using (var Db = new BD2_2Db())
+			{
+				var patient = from patients in Db.Patient
+							  join address in Db.Address
+							  on patients.address_id equals address.Address_id
+							  where patients.Patient_id == id
+							  select new PatientData
+							  {
+								  Patient_id = patients.Patient_id,
+								  First_name = patients.First_name,
+								  Last_name = patients.Last_name,
+								  PESEL = patients.PESEL,
+								  Phone_number = patients.Phone_number,
+								  City = address.City,
+								  Street = address.Street,
+								  HouseNo = address.House_number,
+								  FlatNo = address.Flat_number
+								  //address = address.Flat_number != null ? address.City + " " + address.Street + " " + address.House_number + " " + address.Flat_number : address.City + " " + address.Street + " " + address.House_number
+							  };
+				//var patients = Db.Patient.ToList();
+				return patient.FirstOrDefault();
+			}
+		}
+
+		public static void deleteUser(int id)
+		{
+			using (var Db = new BD2_2Db())
+			{
+				var patient = Db.Patient.Where(p => p.Patient_id == id).FirstOrDefault();
+				if (patient == null)
+					throw new Exception("No such patient!");
+
+				//Db.Address.Remove(add.First()); - adres zostaje w bazie! - co jeśli ktoś inny też go ma?
+				Db.Patient.Remove(patient);
+				Db.SaveChanges();
+			}
+
+		}
+
+
+		public static void setPatientData(String name, String surname, String pesel, String phone, String city, String street, int houseNo, int? flatNo, int? id)
+		{
+			Address address = new Address { City = city, Street = street, House_number = houseNo, Flat_number = flatNo };
+
+			Patient patient = new Patient { First_name = name, Last_name = surname, PESEL = pesel, Phone_number = phone };
+
+			using (var Db = new BD2_2Db())
+			{
+				var matchAddresses = Db.Address
+					.Where(add =>
+						 (add.City == null ? address.City == null : add.City.Equals(address.City))
+							&& (add.Street == null ? address.Street == null : add.Street.Equals(address.Street))
+							&& add.House_number == address.House_number
+							&& (!add.Flat_number.HasValue ? !address.Flat_number.HasValue : add.Flat_number.Value == address.Flat_number.Value));
+				if (matchAddresses.Count() == 0)
+				{
+					Db.Address.Add(address);
+					//Db.SaveChanges(); // ważne!
+				}
+				else
+					address = matchAddresses.First();
+
+				patient.Address = address;
+				Db.Patient.Add(patient);
+				Db.SaveChanges();
+			}
+		}
+
+		public static void editPatientData(String name, String surname, String pesel, String phone, String city, String street, int houseNo, int flatNo, int id)
+		{
+			using (var Db = new BD2_2Db())
+			{
+				var patientData = Db.Patient.Where(p => p.Patient_id == id).FirstOrDefault();
+				patientData.First_name = name;
+				patientData.Last_name = surname;
+				patientData.PESEL = pesel;
+				patientData.Phone_number = phone;
+
+				var patientAddress = Db.Address.Where(p => p.Address_id == patientData.address_id).FirstOrDefault();
+				patientAddress.City = city;
+				patientAddress.Street = street;
+				patientAddress.House_number = houseNo;
+				patientAddress.Flat_number = flatNo;
+				Db.SaveChanges();
+			}
+		}
+	}
+
+	static public class ExaminationDictionary
+	{
+		public static void insertExaminationData(String name, String code, String type)
+		{
+			Examination_dictionary exam = new Examination_dictionary { Examiantion_type = type, Examination_code = code, Examination_name = name };
+			using (var db = new BD2_2Db())
+			{
+				var matchExam = db.Examination_dictionary
+					.Where(ex =>
+					(ex.Examination_code == null ? exam.Examination_code == null : ex.Examination_code.Equals(exam.Examination_code)
+					&& ex.Examination_name == null ? exam.Examination_name == null : ex.Examination_name.Equals(exam.Examination_name)
+					&& ex.Examiantion_type == null ? exam.Examiantion_type == null : ex.Examiantion_type == exam.Examiantion_type));
+				if (matchExam.Count() == 0)
+				{
+					db.Examination_dictionary.Add(exam);
+					db.SaveChanges();
+				}
+			}
+		}
+
+		public static void editExaminationData(String name, String code, String type)
+		{
+			using (var Db = new BD2_2Db())
+			{
+				var examData = Db.Examination_dictionary.Where(e => e.Examination_code == code).FirstOrDefault();
+				examData.Examiantion_type = type;
+				examData.Examination_code = code;
+				examData.Examination_name = name;
+				Db.SaveChanges();
+			}
+		}
+	}
+
+	static public class Patients
+	{
+
+	}
 
 
 }
--- BD2_demaOkien/ExaminationsDictionariesDetails.cs
+++ BD2_demaOkien/ExaminationsDictionariesDetails.cs
@@ -10,25 +10,72 @@ using System.Windows.Forms;
 
 namespace BD2_demaOkien
 {
-    public partial class ExaminationsDictionariesDetails : Form
-    {
-        public ExaminationsDictionariesDetails(ViewMode mode)
-        {
-            InitializeComponent();
-            if (mode == ViewMode.CREATE)
-                Text = "Dodaj badanie";
-            else if (mode == ViewMode.EDIT)
-                Text = "Edytuj badanie";
-        }
+	public partial class ExaminationsDictionariesDetails : Form
+	{
+		private List<String> codes;
+		private List<String> names;
+		private List<String> types;
+		private ViewMode mode_mode;
 
-        private void buttonApply_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+		public ExaminationsDictionariesDetails(ViewMode mode)
+		{
+			InitializeComponent();
+			mode_mode = mode;
+			if (mode == ViewMode.CREATE)
+				Text = "Dodaj badanie";
+			else if (mode == ViewMode.EDIT)
+				Text = "Edytuj badanie";
 
-        private void buttonCancel_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
-    }
+			using (var Db = new BD2_demaOkien.Data.BD2_2Db())
+			{
+				var dict = from exam in Db.Examination_dictionary
+						   select new
+						   {
+							   name = exam.Examination_name,
+							   code = exam.Examination_code,
+							   type = exam.Examiantion_type
+						   };
+				codes = dict.Select(a => a.code).ToList();
+				names = dict.Select(b => b.name).ToList();
+				types = dict.Select(b => b.type).ToList();
+			}
+			textBox2.AutoCompleteMode = AutoCompleteMode.Append;
+			textBox2.AutoCompleteSource = AutoCompleteSource.CustomSource;
+			AutoCompleteStringCollection col_code = new AutoCompleteStringCollection();
+			textBox1.AutoCompleteMode = AutoCompleteMode.Append;
+			textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+			AutoCompleteStringCollection col_name = new AutoCompleteStringCollection();
+			foreach (string c in codes)
+			{
+				col_code.Add(c);
+			}
+			foreach(string c in names)
+			{
+				col_name.Add(c);
+			}
+			textBox2.AutoCompleteCustomSource = col_code;
+			textBox1.AutoCompleteCustomSource = col_name;
+		}
+
+		private void buttonApply_Click(object sender, EventArgs e)
+		{
+			switch (mode_mode)
+			{
+				case ViewMode.CREATE:
+					BD2_demaOkien.ExaminationDictionary.insertExaminationData(textBox1.Text, textBox2.Text, radioButton1.Checked ? "F" : "L");
+					break;
+				case ViewMode.EDIT:
+					BD2_demaOkien.ExaminationDictionary.editExaminationData(textBox1.Text, textBox2.Text, radioButton1.Checked ? "F" : "L");
+					break;
+				default:
+					break;
+			}
+			Close();
+		}
+
+		private void buttonCancel_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
 }
